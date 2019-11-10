cbea39fe641db2a03ba32993f7f7a569c55c57f6
AvengerEug <eugenesumarry@163.com>
2019-11-10 21:49:26 +0800

    新增@Autowired和@Resource注解的区别

--- ioc/src/main/java/com/eugene/sumarry/ioc/annotationtype/UserService.java
+++ ioc/src/main/java/com/eugene/sumarry/ioc/annotationtype/UserService.java
@@ -6,12 +6,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    /**
+     * @Autowired默认是根据byType的方式依赖注入, 若byType的类型的实例不止一个(内部把异常吃掉), 则会根据byName的方式来注入(也就是变成@Resource功能),
+     * 此时是根据属性名来注入的, 它会将属性名首字母大写, 前面添加set关键字变成set方法, 然后利用反射调用set方法完成注入, 所以此时的 ***属性名*** 很重要,
+     * 与自己添加的set方法无关. 所以此时的byName依赖注入方式与xml配置的byName又有差异, 因为xml配置依赖注入的byName方式是根据显示的set方法名决定的。
+     */
     @Autowired
-    private UserDao userDao;
+    private UserDao userDaoImpl1;
 
     @Autowired
     private String name;
 
     @Autowired
     private IndexDao indexDao;
+
 }
